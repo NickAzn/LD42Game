8b66b69f4b3ddbf54b607965b9c1e904e4b5e48d
NickAzn <nicholaslam16@gmail.com>
2018-08-10 16:17:30 -0700

    Enemy

---
+++ LD42/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    public Transform target;
+    public float movementSpeed;
+
+    public GameObject deathObject;
+
+    //float selfDestructTimer;
+
+	// Update is called once per frame
+	void Update () {
+        
+        //Face the target
+        float z = Mathf.Atan2((target.position.y - transform.position.y), (target.position.x - transform.position.x)) * Mathf.Rad2Deg - 90;
+        transform.eulerAngles = new Vector3(0, 0, z);
+
+        //Move forwards
+        transform.position += transform.up * movementSpeed * Time.deltaTime;
+
+        /*
+        selfDestructTimer += Time.deltaTime;
+        if (selfDestructTimer > 5f) {
+            Die();
+        }
+        */
+    }
+
+    public void Die() {
+        GameObject deathObj = Instantiate(deathObject);
+        Vector3 gridPos = new Vector3((int)transform.position.x, (int)transform.position.y, 0);
+        deathObj.transform.position = gridPos;
+        Destroy(gameObject);
+    }
+}
--- LD42/Assets/Scripts/PlayerController.cs
+++ LD42/Assets/Scripts/PlayerController.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    public float movementSpeed;
+
 	// Update is called once per frame
 	void Update () {
         float xMove = Input.GetAxis("Horizontal");
         float yMove = Input.GetAxis("Vertical");
 
-        transform.position += Vector3.right * xMove;
-        transform.position += Vector3.up * yMove;
+        //Move
+        transform.position += Vector3.right * xMove * movementSpeed * Time.deltaTime;
+        transform.position += Vector3.up * yMove * movementSpeed * Time.deltaTime;
 	}
 }
