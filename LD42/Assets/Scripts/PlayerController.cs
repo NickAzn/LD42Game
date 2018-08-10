1068c08aaa9b39cf2b59755e3d29b5638e4c4ae2
NickAzn <nicholaslam16@gmail.com>
2018-08-10 15:43:13 -0700

    Added basic movement

---
+++ LD42/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        float xMove = Input.GetAxis("Horizontal");
+        float yMove = Input.GetAxis("Vertical");
+
+        transform.position += Vector3.right * xMove;
+        transform.position += Vector3.up * yMove;
+	}
+}
